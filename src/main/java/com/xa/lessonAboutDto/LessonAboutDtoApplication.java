7bae7bdc85d6893045495be663b8de3c53dc77df
XAbdulfayz <usa.xamidov@gmail.com>
2023-09-10 12:51:23 +0500

    initial

---
+++ src/main/java/com/xa/lessonAboutDto/LessonAboutDtoApplication.java
@@ -0,0 +1,13 @@
+package com.xa.lessonAboutDto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonAboutDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonAboutDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xa/lessonAboutDto/LessonAboutDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xa.lessonAboutDto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonAboutDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
