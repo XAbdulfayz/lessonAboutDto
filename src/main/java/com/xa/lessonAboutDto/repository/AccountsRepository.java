8b3717bc56b5c5f672b924f95225562cfa9627be
XAbdulfayz <usa.xamidov@gmail.com>
2023-09-10 14:05:32 +0500

    Repositories added

---
+++ src/main/java/com/xa/lessonAboutDto/repository/AccountsRepository.java
@@ -0,0 +1,10 @@
+package com.xa.lessonAboutDto.repository;
+
+import com.xa.lessonAboutDto.etity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+
+
+}
---
+++ src/main/java/com/xa/lessonAboutDto/repository/PhotoRepository.java
@@ -0,0 +1,7 @@
+package com.xa.lessonAboutDto.repository;
+
+import com.xa.lessonAboutDto.etity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+}
